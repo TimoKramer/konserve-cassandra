85cb0a9d99975b48f2b3b4d58c313524c46caca0
Timo Kramer <fw-git@timokramer.de>
2021-03-20 20:50:12 +0100

    WIP

--- src/konserve_cassandra/core.clj
+++ src/konserve_cassandra/core.clj
@@ -1,55 +1,155 @@
 (ns konserve-cassandra.core
   "Address globally aggregated immutable key-value store(s)."
-  (:require [clojure.core.async :as async]
-            [hasch.core :as hasch]
-            [konserve.protocols :refer [PEDNAsyncKeyValueStore
-                                        -exists? -get -get-meta
-                                        -update-in -assoc-in -dissoc
-                                        PBinaryAsyncKeyValueStore
-                                        -bassoc -bget
-                                        -serialize -deserialize
-                                        PKeyIterable
-                                        -keys]]
-            [konserve.serializers :as ser]
-            [konserve.compressor :as com]
-            [konserve.encryptor :as encr]
+  (:require [clojure.core.async :refer [<!! thread chan put!]]
+            [hasch.core :as h]
+            [konserve
+             [protocols :refer [PEDNAsyncKeyValueStore
+                                -exists? -get -get-meta
+                                -update-in -assoc-in -dissoc
+                                PBinaryAsyncKeyValueStore
+                                -bassoc -bget
+                                -serialize -deserialize
+                                PKeyIterable
+                                -keys]]
+             [serializers :as ks]
+             [compressor :as kc]
+             [core :as k]]
             [incognito.edn :refer [read-string-safe]]
-            [konserve-cassandra.io :as io]))
+            [konserve-cassandra.io :as io])
+  (:import [java.io ByteArrayOutputStream ByteArrayInputStream]
+           [java.nio ByteBuffer]))
 
 (set! *warn-on-reflection* 1)
+
+(def version 1)
+
+(def encrypt 0)
 
 (defn prep-ex
   [^String message ^Exception e]
   ; (.printStackTrace e)
   (ex-info message {:error (.getMessage e) :cause (.getCause e) :trace (.getStackTrace e)}))
 
+(defn str-uuid
+  [key]
+  (str (h/uuid key)))
+
+(defn to-byte-array
+  "Return 4 Byte Array with following content
+     1th Byte = Version of Konserve
+     2th Byte = Serializer Type
+     3th Byte = Compressor Type
+     4th Byte = Encryptor Type"
+  [version serializer compressor encryptor]
+  (let [env-array     (byte-array [version serializer compressor encryptor])
+        return-buffer (ByteBuffer/allocate 4)
+        _             (.put return-buffer env-array)
+        return-array  (.array return-buffer)]
+    (.clear return-buffer)
+    return-array))
+
+(defn write-edn [edn serializer compressor write-handlers]
+  (let [bos           (ByteArrayOutputStream.)
+        serializer-id (get ks/serializer-class->byte (type serializer))
+        compressor-id (get kc/compressor->byte compressor)
+        _             (.write bos (to-byte-array version serializer-id compressor-id encrypt))
+        _             (-serialize (compressor serializer) bos write-handlers edn)]
+    (.toByteArray bos)))
+
+(defn read-edn [ba serializers read-handlers]
+  (let [serializer-id  (aget ba 1)
+        compressor-id  (aget ba 2)
+        serializer-key (ks/byte->key serializer-id)
+        serializer     (get serializers serializer-key)
+        compressor     (kc/byte->compressor compressor-id)
+        bis            (ByteArrayInputStream. ba 4 (count ba))
+        edn            (-deserialize (compressor serializer) read-handlers bis)]
+    [edn serializer]))
+
+(comment
+  (def bos (ByteArrayOutputStream.))
+  (def serializer-id 1)
+  (def compressor-id 1)
+  (def write-handler (atom {}))
+  (.write bos (to-byte-array 1 1 1 0))
+  (-serialize (kc/lz4-compressor (ks/fressian-serializer)) bos write-handler {:foo :bar})
+  (def ba (.toByteArray bos))
+
+  (def serializers (merge ks/key->serializer nil))
+  (def serializer-key (ks/byte->key serializer-id))
+  (def serializer (get serializers serializer-key))
+  (def read-handlers (atom {}))
+  (def compressor (kc/byte->compressor compressor-id))
+  (def bis (ByteArrayInputStream. ba 4 (count ba)))
+  (def edn (-deserialize (kc/lz4-compressor serializer) read-handlers bis)))
+
+(defrecord CassandraStore [connection cluster default-serializer serializers compressor encryptor read-handlers write-handlers]
+  PEDNAsyncKeyValueStore
+
+  (-exists? [_ k]
+    (io/exists? connection (str-uuid k)))
+
+  (-get [_ k]
+    (let [result (<!! (io/get-data connection (str-uuid k)))]
+      (read-edn result serializers read-handlers)))
+
+  (-get-meta [_ k]
+    (let [result (<!! (io/get-meta connection (str-uuid k)))]
+      (read-edn result serializers read-handlers)))
+
+  (-update-in [_ key-vec meta-update-fn update-fn args]
+    (let [[fkey & rkey] key-vec
+          id (str-uuid fkey)
+          {:keys [id data meta]} (<!! (io/get-all connection id))
+          old-data (read-edn data serializers read-handlers)
+          old-meta (read-edn meta serializers read-handlers)
+          new-data (if (empty? rkey)
+                     (apply update-fn old-data args)
+                     (apply update-in old-data rkey update-fn args))
+          new-meta (meta-update-fn old-meta)
+          ba-data (write-edn new-data serializer compressor write-handlers)
+          ba-meta (write-edn new-meta serializer compressor write-handlers)]
+      (io/insert connection id [ba-meta ba-data])))
+
+  (-assoc-in [this key-vec meta val]
+    (-update-in this key-vec meta (fn [_] val) []))
+
+  (-dissoc [_ k]
+    (io/delete connection (str-uuid k))))
+
+(comment
+  (def ba2 (:data (<!! (io/get-data io/conn (str-uuid :foo)))))
+  (def bis2 (ByteArrayInputStream. ba2))
+  (def bis2 (ByteArrayInputStream. ba2 4 (count ba2))))
+
 (defn new-cassandra-store
   "Creates an new Cassandra konserve store."
-  [config & {:keys [table serializer compressor encryptor read-handlers write-handlers]
-             :or   {table "konserve"
-                    serializer :FressianSerializer
+  [config & {:keys [table default-serializer serializers compressor encryptor read-handlers write-handlers]
+             :or   {default-serializer :FressianSerializer
+                    table "konserve"
                     compressor com/lz4-compressor
-                    encryptor encr/null-encryptor
+                    encryptor enc/null-encryptor
                     read-handlers (atom {})
                     write-handlers (atom {})}}]
-  (let [res-ch (async/chan 1)]
-    (async/thread
+  (let [res-ch (chan 1)]
+    (thread
       (try
         (let [cluster (io/cluster (:cluster config))
-              connection (io/connect cluster)]
-          (async/put! res-ch
-                      #_(map->CassandraStore)
-                      {:connection connection
-                       :cluster cluster
-                       :table table
-                       :serializer serializer
-                       :compressor compressor
-                       :encryptor encryptor
-                       :read-handlers read-handlers
-                       :write-handlers write-handlers
-                       :locks (atom {})}))
+              session (io/connect cluster (-> config
+                                              :cluster
+                                              :session-keyspace))]
+          (put! res-ch
+                      (map->CassandraStore
+                        {:connection {:session session :table table}
+                         :cluster cluster
+                         :default-serializer default-serializer
+                         :serializers (merge ser/key->serializer serializers)
+                         :compressor compressor
+                         :encryptor encryptor
+                         :read-handlers read-handlers
+                         :write-handlers write-handlers})))
         (catch Exception e
-          (async/put! res-ch
+          (put! res-ch
                       (prep-ex "Failed to connect to store." e)))))
     res-ch))
 
@@ -58,5 +158,10 @@
   (io/drop-table conn))
 
 (comment
-  (async/<!! (new-cassandra-store {:cluster {:session-keyspace "alia"
-                                             :contact-points ["127.0.0.1"]}})))
+  (require '[konserve.core :as k])
+  (<!! (k/assoc io/conn :foo :bar))
+  (<!! (k/get io/conn :foo))
+  (<!! (k/assoc-in io/conn [:baz] {:bar 42}))
+  (<!! (k/update-in io/conn [:foo] :name))
+  (<!! (k/get io/conn :foo))
+  (io/insert io/conn (str-uuid :foo) [(write-edn :data serializer compressor write-handler) (write-edn :meta serializer compressor write-handler)]))
--- src/konserve_cassandra/io.clj
+++ src/konserve_cassandra/io.clj
@@ -2,44 +2,72 @@
   (:require [qbits.alia :as qa]
             [qbits.alia.async :as qasync]
             [qbits.hayt :as qh]
-            [clojure.core.async :as async])
-  (:import  [java.io ByteArrayInputStream]))
+            [clojure.core.async :as async]
+            [konserve-cassandra.core :as core])
+  (:import  [java.io ByteArrayInputStream ByteArrayOutputStream]))
 
-(defn split-header [bytes-or-blob dbtype]
-  (when (some? bytes-or-blob) 
-    (let [data  (->> bytes vec (split-at 4))
+(defn split-header [bytes-or-blob]
+  (when (some? bytes-or-blob)
+    (let [data  (->> bytes-or-blob vec (split-at 4))
           streamer (fn [header data] (list (byte-array header) (-> data byte-array (ByteArrayInputStream.))))]
       (apply streamer data))))
 
 (defn cluster [cluster-config]
   (qa/cluster cluster-config))
 
-(defn connect [cluster]
-  (qa/connect cluster))
+(defn connect [cluster keyspace]
+  (qa/connect cluster keyspace))
 
-; TODO enough to check on any table?
-(defn it-exists?
+(defn exists?
   "Check if the Cassandra connection has any tables in its keyspace
-     
-   Argument: connection map"
-  [{:keys [session]}]
-  (-> (qa/execute session (qh/select :system_schema.tables
-                                     (qh/columns :table_name)
-                                     (qh/where {:keyspace_name (.getLoggedKeyspace session)})))
-      (empty?)
-      (not))) ; returns empty list so checking on not emtpy
 
-(defn get-it [conn id]
-  (let [res' (qa/execute (:session conn) (qh/select (:table conn) (qh/where {:id id})))
-        data (:data res')
-        meta (:meta res')
-        res (if (and meta data)
-              [(split-header meta (-> conn :db :dbtype)) (split-header data  (-> conn :db :dbtype))]
-              [nil nil])]
-    res))
+   Arguments: session, table, id"
+  [{:keys [session table]} id]
+  (qasync/execute-chan session
+                       (qh/select table
+                                  (qh/where {:id id}))))
+
+(defn get-all [{:keys [session table]} id]
+  (qasync/execute-chan session
+                       (qh/select table
+                         (qh/where {:id id}))
+                       {:result-set-fn #(first %)}))
+
+(defn get-data [{:keys [session table]} id]
+  (qasync/execute-chan session
+                       (qh/select table
+                                  (qh/columns :id :data)
+                                  (qh/where {:id id}))
+                       {:result-set-fn #(first %)}))
+
+(defn get-meta [{:keys [session table]} id]
+  (qasync/execute-chan session
+                       (qh/select table
+                                  (qh/columns :id :meta)
+                                  (qh/where {:id id}))
+                       {:result-set-fn #(first %)}))
+
+(defn update [{:keys [session table]} id data]
+  (qa/execute session
+              (qh/update table
+                         (qh/set-columns {:meta (first data)
+                                          :data (second data)})
+                         (qh/where {:id id}))))
+
+(defn insert [{:keys [session table]} id data]
+  (qa/execute session
+              (qh/insert table
+                         (qh/values {:id id
+                                     :meta (first data)
+                                     :data (second data)}))))
+
+(defn delete [{:keys [session table]} id]
+  (qasync/execute-chan session
+                       (qh/delete table
+                                  (qh/where {:id id}))))
 
 (defn create-table [{:keys [session table]}]
-  (qa/execute session 
+  (qa/execute session
               (qh/create-table table
                                (qh/if-not-exists)
                                (qh/column-definitions {:id :varchar
@@ -58,37 +86,26 @@
   (def conn {:session (qa/connect cluster "alia")
              :table :foo})
 
-  (.getLoggedKeyspace (qa/connect cluster))
+  (.getLoggedKeyspace (:session conn))
 
   (create-table conn)
-  (it-exists? conn)
+  (async/<!! (exists? conn "1"))
   (drop-table conn)
 
-  (qa/execute session "CREATE KEYSPACE alia WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};")
+  (async/<!! (qasync/execute-chan (:session conn) "SELECT * FROM foo WHERE id = '1'"))
 
-  (qa/execute session "CREATE TABLE users (user_name varchar,
-                                           first_name varchar,
-                                           last_name varchar,
-                                           auuid uuid,
-                                           birth_year bigint,
-                                           created timestamp,
-                                           valid boolean,
-                                           emails set<text>,
-                                           tags list<bigint>,
-                                           amap map<varchar, bigint>,
-                                           PRIMARY KEY (user_name));")
+  (require '[konserve-cassandra.core :as core])
+  (def ^ByteArrayOutputStream test-meta (ByteArrayOutputStream.))
+  (.write test-meta 1)
+  (def ^ByteArrayOutputStream test-data (ByteArrayOutputStream.))
+  (.write test-data 3)
+  (update conn (core/str-uuid [:foo :bar]) [(.toByteArray test-meta) (.toByteArray test-data)])
+  (qa/execute (:session conn)
+              (qh/select :foo))
 
-  (qa/execute session "INSERT INTO users
-                       (user_name, first_name, last_name, emails, birth_year, amap, tags, auuid,valid)
-                       VALUES('frodo', 'Frodo', 'Baggins',
-                       {'f@baggins.com', 'baggins@gmail.com'}, 1,
-                       {'foo': 1, 'bar': 2}, [4, 5, 6],
-                       1f84b56b-5481-4ee4-8236-8a3831ee5892, true);")
-
-  (def prepared-statement (qa/prepare session "select * from users where user_name=?;"))
-
-  (qa/execute session prepared-statement {:values ["frodo"]})
-
-  (def prepared-statement (qa/prepare session "select * from users where user_name= :name limit :lmt;"))
-
-  (qa/execute session prepared-statement {:values {:name "frodo" :lmt (int 1)}}))
+  (:data (async/<!! (get-all conn "02d2b7ff-609b-50c4-9437-9c7eb6e4e98f")))
+  (async/<!! (get-data conn "02d2b7ff-609b-50c4-9437-9c7eb6e4e98f"))
+  (qa/execute (:session conn)
+              (qh/select (:table conn)
+                (qh/where {:id "02d2b7ff-609b-50c4-9437-9c7eb6e4e98f"}))
+              {:result-set-fn #(first %)}))
