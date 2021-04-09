f92fe8895db655b199da23e4dda8a83195136e8c
Timo Kramer <fw-git@timokramer.de>
2021-04-09 14:06:07 +0200

    Add all the functionality and some tests

--- src/konserve_cassandra/core.clj
+++ src/konserve_cassandra/core.clj
@@ -1,5 +1,5 @@
 (ns konserve-cassandra.core
-  (:require [clojure.core.async :refer [<!! thread put! close! chan]]
+  (:require [clojure.core.async :refer [<!! thread put! close! chan] :as async]
             [konserve.serializers :as ser]
             [konserve.compressor :as comp]
             [konserve.encryptor :as encr]
@@ -36,7 +36,7 @@
 (defrecord CassandraStore [conn default-serializer serializers compressor encryptor read-handlers write-handlers locks]
   PEDNAsyncKeyValueStore
   (-exists?
-    [this key]
+    [_ key]
     (let [res-ch (chan 1)]
       (thread
         (try
@@ -45,7 +45,7 @@
       res-ch))
 
   (-get
-    [this key]
+    [_ key]
     (let [res-ch (chan 1)]
       (thread
         (try
@@ -62,7 +62,7 @@
       res-ch))
 
   (-get-meta
-    [this key]
+    [_ key]
     (let [res-ch (chan 1)]
       (thread
         (try
@@ -79,7 +79,7 @@
       res-ch))
 
   (-update-in
-    [this key-vec meta-up-fn up-fn args]
+    [_ key-vec meta-up-fn up-fn args]
     (let [res-ch (chan 1)]
       (thread
         (try
@@ -115,144 +115,143 @@
               (.write vbaos ^byte (comp/compressor->byte compressor))
               (.write vbaos ^byte (encr/encryptor->byte encryptor))
               (-serialize writer vbaos write-handlers nval))
-            (if (first old-val)
-              (io/update-both conn (str-uuid fkey) [(.toByteArray mbaos) (.toByteArray vbaos)])
-              (io/insert-both conn (str-uuid fkey) [(.toByteArray mbaos) (.toByteArray vbaos)]))
+            (io/update-both conn (str-uuid fkey) [(.toByteArray mbaos) (.toByteArray vbaos)])
             (put! res-ch [(second old-val) nval]))
-          (catch Exception e (put! res-ch (prep-ex "Failed to update/write value in store" e))))
-        res-ch)))
+          (catch Exception e (put! res-ch (prep-ex "Failed to update/write value in store" e)))))
+      res-ch))
 
   (-assoc-in [this key-vec meta val] (-update-in this key-vec meta (fn [_] val) []))
 
   (-dissoc
-    [this key]
+    [_ key]
     (let [res-ch (chan 1)]
       (thread
         (try
           (io/delete-entry conn (str-uuid key))
           (close! res-ch)
-          (catch Exception e (put! res-ch (prep-ex "Failed to delete key-value pair from store" e))))
-        res-ch))))
+          (catch Exception e (put! res-ch (prep-ex "Failed to delete key-value pair from store" e)))))
+      res-ch))
 
-  ;PBinaryAsyncKeyValueStore
-  ;(-bget
-  ;  [this key locked-cb]
-  ;  (let [res-ch (chan 1)]
-  ;    (thread
-  ;      (try
-  ;        (let [[header res] (io/get-data conn (str-uuid key))]
-  ;          (if (some? res)
-  ;            (let [rserializer (ser/byte->serializer (get header 1))
-  ;                  rcompressor (comp/byte->compressor (get header 2))
-  ;                  rencryptor  (encr/byte->encryptor  (get header 3))
-  ;                  reader (-> rserializer rencryptor rcompressor)
-  ;                  data (-deserialize reader read-handlers res)]
-  ;              (put! res-ch (locked-cb (prep-stream data))))
-  ;            (close! res-ch)))
-  ;        (catch Exception e (put! res-ch (prep-ex "Failed to retrieve binary value from store" e)))))
-  ;    res-ch))
+  PBinaryAsyncKeyValueStore
+  (-bget
+    [_ key locked-cb]
+    (let [res-ch (chan 1)]
+      (thread
+        (try
+          (let [[header res] (io/get-data conn (str-uuid key))]
+            (if (some? res)
+              (let [rserializer (ser/byte->serializer (get header 1))
+                    rcompressor (comp/byte->compressor (get header 2))
+                    rencryptor (encr/byte->encryptor  (get header 3))
+                    reader (-> rserializer rencryptor rcompressor)
+                    data (-deserialize reader read-handlers res)]
+                (put! res-ch (locked-cb (prep-stream data))))
+              (close! res-ch)))
+          (catch Exception e (put! res-ch (prep-ex "Failed to retrieve binary value from store" e)))))
+     res-ch))
 
-  ;(-bassoc
-  ;  [this key meta-up-fn input]
-  ;  (let [res-ch (chan 1)]
-  ;    (thread
-  ;      (try
-  ;        (let [[[mheader old-meta'] [_ old-val]] (io/get-all conn (str-uuid key))
-  ;              old-meta (when old-meta'
-  ;                          (let [mserializer (ser/byte->serializer  (get mheader 1))
-  ;                                mcompressor (comp/byte->compressor (get mheader 2))
-  ;                                mencryptor  (encr/byte->encryptor  (get mheader 3))
-  ;                                reader (-> mserializer mencryptor mcompressor)]
-  ;                            (-deserialize reader read-handlers old-meta')))
-  ;              new-meta (meta-up-fn old-meta)
-  ;              serializer (get serializers default-serializer)
-  ;              writer (-> serializer compressor encryptor)
-  ;              ^ByteArrayOutputStream mbaos (ByteArrayOutputStream.)
-  ;              ^ByteArrayOutputStream vbaos (ByteArrayOutputStream.)]
-  ;          (when new-meta
-  ;            (.write mbaos ^byte store-layout)
-  ;            (.write mbaos ^byte (ser/serializer-class->byte (type serializer)))
-  ;            (.write mbaos ^byte (comp/compressor->byte compressor))
-  ;            (.write mbaos ^byte (encr/encryptor->byte encryptor))
-  ;            (-serialize writer mbaos write-handlers new-meta))
-  ;          (when input
-  ;            (.write vbaos ^byte store-layout)
-  ;            (.write vbaos ^byte (ser/serializer-class->byte (type serializer)))
-  ;            (.write vbaos ^byte (comp/compressor->byte compressor))
-  ;            (.write vbaos ^byte (encr/encryptor->byte encryptor))
-  ;            (-serialize writer vbaos write-handlers input))
-  ;          (if old-meta
-  ;            (io/update-both conn (str-uuid key) [(.toByteArray mbaos) (.toByteArray vbaos)])
-  ;            (io/insert-both conn (str-uuid key) [(.toByteArray mbaos) (.toByteArray vbaos)]))
-  ;          (put! res-ch [old-val input]))
-  ;        (catch Exception e (put! res-ch (prep-ex "Failed to write binary value in store" e))))
-  ;      res-ch))))
+  (-bassoc
+    [_ key meta-up-fn input]
+    (let [res-ch (chan 1)]
+      (thread
+        (try
+          (let [[[mheader old-meta'] [_ old-val]] (io/get-both conn (str-uuid key))
+                old-meta (when old-meta'
+                            (let [mserializer (ser/byte->serializer  (get mheader 1))
+                                  mcompressor (comp/byte->compressor (get mheader 2))
+                                  mencryptor (encr/byte->encryptor  (get mheader 3))
+                                  reader (-> mserializer mencryptor mcompressor)]
+                              (-deserialize reader read-handlers old-meta')))
+                new-meta (meta-up-fn old-meta)
+                serializer (get serializers default-serializer)
+                writer (-> serializer compressor encryptor)
+                ^ByteArrayOutputStream mbaos (ByteArrayOutputStream.)
+                ^ByteArrayOutputStream vbaos (ByteArrayOutputStream.)]
+            (when new-meta
+              (.write mbaos ^byte store-layout)
+              (.write mbaos ^byte (ser/serializer-class->byte (type serializer)))
+              (.write mbaos ^byte (comp/compressor->byte compressor))
+              (.write mbaos ^byte (encr/encryptor->byte encryptor))
+              (-serialize writer mbaos write-handlers new-meta))
+            (when input
+              (.write vbaos ^byte store-layout)
+              (.write vbaos ^byte (ser/serializer-class->byte (type serializer)))
+              (.write vbaos ^byte (comp/compressor->byte compressor))
+              (.write vbaos ^byte (encr/encryptor->byte encryptor))
+              (-serialize writer vbaos write-handlers input))
+            (if old-meta
+              (io/update-both conn (str-uuid key) [(.toByteArray mbaos) (.toByteArray vbaos)])
+              (io/insert-both conn (str-uuid key) [(.toByteArray mbaos) (.toByteArray vbaos)]))
+            (put! res-ch [old-val input]))
+          (catch Exception e (put! res-ch (prep-ex "Failed to write binary value in store" e)))))
+      res-ch))
 
-  ;PKeyIterable
-  ;(-keys
-  ;  [_]
-  ;  (let [res-ch (chan)]
-  ;    (thread
-  ;      (try
-  ;        (let [key-stream (io/get-keys conn)
-  ;              keys' (when key-stream
-  ;                      (for [[header k] key-stream]
-  ;                        (let [rserializer (ser/byte->serializer (get header 1))
-  ;                              rcompressor (comp/byte->compressor (get header 2))
-  ;                              rencryptor  (encr/byte->encryptor  (get header 3))
-  ;                              reader (-> rserializer rencryptor rcompressor)]
-  ;                          (-deserialize reader read-handlers k))))
-  ;              keys (doall (map :key keys'))]
-  ;          (doall
-  ;            (map #(put! res-ch %) keys))
-  ;          (close! res-ch))
-  ;        (catch Exception e (put! res-ch (prep-ex "Failed to retrieve keys from store" e))))
-  ;      res-ch)))
+  PKeyIterable
+  (-keys
+    [_]
+    (let [res-ch (chan)]
+      (thread
+        (try
+          (let [key-stream (io/get-keys conn)
+                keys' (when key-stream
+                        (for [[header k] key-stream]
+                          (let [rserializer (ser/byte->serializer (get header 1))
+                                rcompressor (comp/byte->compressor (get header 2))
+                                rencryptor  (encr/byte->encryptor (get header 3))
+                                reader (-> rserializer rencryptor rcompressor)]
+                            (-deserialize reader read-handlers k))))]
+            (doall (map #(put! res-ch (:key %)) keys'))
+            (close! res-ch))
+          (catch Exception e (put! res-ch (ex-info "Failed to retrieve keys from store"
+                                                   {:error (.getMessage e) :cause (.getCause e) :trace (.getStackTrace e)})))))
+      res-ch))
 
-  ;SplitLayout
-  ;(-get-raw-meta [this key]
-  ;  (let [res-ch (chan 1)]
-  ;    (thread
-  ;      (try
-  ;        (let [res (io/raw-get-meta conn (str-uuid key))]
-  ;          (if res
-  ;            (put! res-ch res)
-  ;            (close! res-ch)))
-  ;        (catch Exception e (put! res-ch (prep-ex "Failed to retrieve raw metadata from store" e)))))
-  ;    res-ch))
-  ;(-put-raw-meta [this key blob]
-  ;  (let [res-ch (chan 1)]
-  ;    (thread
-  ;      (try
-  ;        (if (io/it-exists? conn (str-uuid key))
-  ;          (io/raw-update-meta conn (str-uuid key) blob)
-  ;          (io/raw-insert-meta conn (str-uuid key) blob))
-  ;        (close! res-ch)
-  ;        (catch Exception e (put! res-ch (prep-ex "Failed to write raw metadata to store" e)))))
-  ;    res-ch))
-  ;(-get-raw-value [this key]
-  ;  (let [res-ch (chan 1)]
-  ;    (thread
-  ;      (try
-  ;        (let [res (io/raw-get-data conn (str-uuid key))]
-  ;          (if res
-  ;            (put! res-ch res)
-  ;            (close! res-ch)))
-  ;        (catch Exception e (put! res-ch (prep-ex "Failed to retrieve raw value from store" e)))))
-  ;    res-ch))
-  ;(-put-raw-value [this key blob]
-  ;  (let [res-ch (chan 1)]
-  ;    (thread
-  ;      (try
-  ;        (if (io/it-exists? conn (str-uuid key))
-  ;          (io/raw-update-data conn (str-uuid key) blob)
-  ;          (io/raw-insert-data conn (str-uuid key) blob))
-  ;        (close! res-ch)
-  ;        (catch Exception e (put! res-ch (prep-ex "Failed to write raw value to store" e)))))
-  ;    res-ch)))
+  SplitLayout
+  (-get-raw-meta [_ key]
+    (let [res-ch (chan 1)]
+      (thread
+        (try
+          (let [res (io/raw-get-meta conn (str-uuid key))]
+            (if res
+              (put! res-ch res)
+              (close! res-ch)))
+          (catch Exception e (put! res-ch (prep-ex "Failed to retrieve raw metadata from store" e)))))
+      res-ch))
 
+  (-put-raw-meta [_ key blob]
+    (let [res-ch (chan 1)]
+      (thread
+        (try
+          (if (io/exists? conn (str-uuid key))
+            (io/raw-update-meta conn (str-uuid key) blob)
+            (io/raw-insert-meta conn (str-uuid key) blob))
+          (close! res-ch)
+          (catch Exception e (put! res-ch (prep-ex "Failed to write raw metadata to store" e)))))
+      res-ch))
 
-(defn new-cassandra-store
+  (-get-raw-value [_ key]
+    (let [res-ch (chan 1)]
+      (thread
+        (try
+          (let [res (io/raw-get-data conn (str-uuid key))]
+            (if res
+              (put! res-ch res)
+              (close! res-ch)))
+          (catch Exception e (put! res-ch (prep-ex "Failed to retrieve raw value from store" e)))))
+      res-ch))
+
+  (-put-raw-value [_ key blob]
+    (let [res-ch (chan 1)]
+      (thread
+        (try
+          (if (io/exists? conn (str-uuid key))
+            (io/raw-update-data conn (str-uuid key) blob)
+            (io/raw-insert-data conn (str-uuid key) blob))
+          (close! res-ch)
+          (catch Exception e (put! res-ch (prep-ex "Failed to write raw value to store" e)))))
+      res-ch)))
+
+(defn new-store
   [config & {:keys [table default-serializer serializers compressor encryptor read-handlers write-handlers]
              :or {default-serializer :FressianSerializer
                   table "konserve"
@@ -266,7 +265,8 @@
         (let [cluster (io/cluster (:cluster config))
               session (io/connect cluster (-> config
                                               :cluster
-                                              :session-keyspace))]
+                                              :session-keyspace))
+              _ (io/create-table {:session session :table table})]
             (put! res-ch
               (map->CassandraStore {:conn {:session session :table table}
                                     :default-serializer default-serializer
@@ -280,11 +280,11 @@
           (put! res-ch (prep-ex "Failed to connect to store" e)))))
     res-ch))
 
-(defn delete-store [connection]
+(defn delete-store [{:keys [conn]}]
   (let [res-ch (chan 1)]
     (thread
       (try
-        (io/drop-table connection)
+        (io/drop-table conn)
         (close! res-ch)
         (catch Exception e (put! res-ch (prep-ex "Failed to delete store" e)))))
     res-ch))
@@ -293,8 +293,9 @@
   (require '[konserve.core :as k])
   (def config {:cluster {:session-keyspace "alia"
                          :contact-points ["127.0.0.1"]}})
-  (def store (<!! (new-cassandra-store config)))
-  (def read-handlers (atom {}))
+  (def store (<!! (new-store config :table "foo")))
+  (def conn (:conn store))
+  (<!! (delete-store store))
   (io/create-table (-> store
                        :conn))
   (<!! (k/exists? store :foo))
@@ -302,4 +303,21 @@
   (<!! (k/get store :foo))
   (<!! (k/assoc-in store [:foo] :bar2))
   (<!! (k/get-in store [:fooo] :default))
-  (<!! (<!! (k/update-in store [:foo] name))))
+  (<!! (k/update-in store [:foo] name))
+
+  (def q (<!! (k/keys store)))
+  (<!! q)
+  (<!! (async/into #{} (k/keys store)))
+
+  (def res-ch (chan))
+  (def key-stream (io/get-keys conn))
+  (def keys' (when key-stream
+               (for [[header k] key-stream]
+                 (let [rserializer (ser/byte->serializer (get header 1))
+                       rcompressor (comp/byte->compressor (get header 2))
+                       rencryptor  (encr/byte->encryptor (get header 3))
+                       reader (-> rserializer rencryptor rcompressor)]
+                   (-deserialize reader (atom {}) k)))))
+  (doall (map :key keys'))
+  (doall (map #(put! res-ch (:key %)) keys'))
+  (close! res-ch))
--- src/konserve_cassandra/io.clj
+++ src/konserve_cassandra/io.clj
@@ -3,20 +3,22 @@
             [qbits.hayt :as qh])
   (:import  [java.io ByteArrayInputStream ByteArrayOutputStream]))
 
-(defn split-header [byte-buffer]
-  (when (some? byte-buffer)
-    (let [data  (->> byte-buffer
-                     .array
-                     vec
-                     (split-at 4))
+(defn split-header [byte-obj]
+  (when (some? byte-obj)
+    (let [data (->> byte-obj
+                    vec
+                    (split-at 4))
           streamer (fn [header data] (list (byte-array header) (-> data byte-array (ByteArrayInputStream.))))]
       (apply streamer data))))
 
 (defn cluster [cluster-config]
   (qa/cluster cluster-config))
 
-(defn connect [cluster keyspace]
-  (qa/connect cluster keyspace))
+(defn connect
+  ([cluster]
+   (qa/connect cluster))
+  ([cluster keyspace]
+   (qa/connect cluster keyspace)))
 
 (defn exists?
   "Check if the Cassandra connection has any tables in its keyspace
@@ -30,30 +32,56 @@
       empty?
       not))
 
-(defn get-both [{:keys [session table]} id]
-  (let [{:keys [data meta id]} (qa/execute session
-                                           (qh/select table
-                                             (qh/where {:id id}))
-                                           {:result-set-fn #(first %)})]
+(defn raw-get-both [{:keys [session table]} id]
+  (let [{:keys [data meta]} (qa/execute session
+                                        (qh/select table
+                                          (qh/where {:id id}))
+                                        {:result-set-fn #(first %)})]
+    (when (and meta data)
+      {:data (.array data)
+       :meta (.array meta)})))
+
+(defn get-both [conn id]
+  (let [{:keys [data meta]} (raw-get-both conn id)]
     (if (and meta data)
       [(split-header meta) (split-header data)]
       [nil nil])))
 
-(defn get-data [{:keys [session table]} id]
+(defn raw-get-data [{:keys [session table]} id]
   (let [{:keys [data]} (qa/execute session
                                    (qh/select table
                                               (qh/columns :data)
                                               (qh/where {:id id}))
                                    {:result-set-fn #(first %)})]
+    (when data
+      (.array data))))
+
+(defn get-data [conn id]
+  (let [data (raw-get-data conn id)]
     (when data (split-header data))))
 
-(defn get-meta [{:keys [session table]} id]
+(defn raw-get-meta [{:keys [session table]} id]
   (let [{:keys [meta]} (qa/execute session
                                    (qh/select table
                                               (qh/columns :meta)
                                               (qh/where {:id id}))
                                    {:result-set-fn #(first %)})]
+    (when meta
+      (.array meta))))
+
+(defn get-meta [conn id]
+  (let [meta (raw-get-meta conn id)]
     (when meta (split-header meta))))
+
+(defn get-keys [{:keys [session table]}]
+  (let [res' (qa/execute session
+                         (qh/select table
+                                    (qh/columns :id :meta)))]
+    (doall (map #(-> %
+                     :meta
+                     (.array)
+                     (split-header))
+                res'))))
 
 (defn update-both [{:keys [session table]} id data]
   (qa/execute session
@@ -69,10 +97,57 @@
                                      :meta (first data)
                                      :data (second data)}))))
 
+(defn raw-update-data [{:keys [session table]} id blob]
+  (qa/execute session
+              (qh/update table
+                         (qh/set-columns {:data blob})
+                         (qh/where {:id id}))))
+
+(defn raw-update-meta [{:keys [session table]} id blob]
+  (qa/execute session
+              (qh/update table
+                         (qh/set-columns {:meta blob})
+                         (qh/where {:id id}))))
+
+(defn raw-insert-data [{:keys [session table]} id blob]
+  (qa/execute session
+              (qh/insert table
+                         (qh/values {:id id
+                                     :data blob}))))
+
+(defn raw-insert-meta [{:keys [session table]} id blob]
+  (qa/execute session
+              (qh/insert table
+                         (qh/values {:id id
+                                     :meta blob}))))
+
 (defn delete-entry [{:keys [session table]} id]
   (qa/execute session
               (qh/delete table
                          (qh/where {:id id}))))
+
+(defn create-keyspace
+  "Takes a session object from connect fn"
+  ([session]
+   (create-keyspace session nil))
+  ([session config]
+   (let [session-keyspace (or (-> config :cluster :session-keyspace)
+                              "konserve")
+         keyspace-config (or (-> config :keyspace)
+                             {:replication {:class "SimpleStrategy" :replication_factor 1}})]
+     (qa/execute session
+                 (qh/create-keyspace session-keyspace
+                                     (qh/with keyspace-config))))))
+
+(defn drop-keyspace
+  "Takes a session object from connect fn"
+  ([session]
+   (drop-keyspace session nil))
+  ([session config]
+   (let [session-keyspace (or (-> config :cluster :session-keyspace)
+                              "konserve")]
+     (qa/execute session
+                 (qh/drop-keyspace session-keyspace)))))
 
 (defn create-table [{:keys [session table]}]
   (qa/execute session
@@ -93,25 +168,30 @@
   (def config {:cluster {:session-keyspace "alia"
                          :contact-points ["127.0.0.1"]}})
 
-  (def cluster (cluster (:cluster config)))
-  (def conn (qa/connect cluster))
-  (qa/execute conn (qh/create-keyspace :alia (qh/with {:replication {:class "SimpleStrategy" :replication_factor 1}})))
+  (def mycluster (cluster (:cluster config)))
+  (def session (qa/connect mycluster))
 
-  (def store (<!! (core/new-cassandra-store config)))
+  (create-keyspace session config)
+  (drop-keyspace session config)
+  (qa/execute session
+              (qh/select "system_schema.keyspaces"))
+
+  (def store (<!! (core/new-store config)))
+  (qa/shutdown (:session (:conn store)))
 
   (.getLoggedKeyspace (:session (:conn store)))
 
-  (create-table conn)
-  (<!! (exists? conn "1"))
-  (drop-table conn)
+  (create-table (:conn store))
+  (exists? (:conn store) "1")
+  (drop-table (:conn store))
 
-  (require '[konserve-cassandra.core :as core])
   (def ^ByteArrayOutputStream test-meta (ByteArrayOutputStream.))
   (.write test-meta 1)
   (def ^ByteArrayOutputStream test-data (ByteArrayOutputStream.))
   (.write test-data 3)
-  (update conn (core/str-uuid [:foo :bar]) [(.toByteArray test-meta) (.toByteArray test-data)])
-  (def foo (qa/execute (:session conn)
+  (update-both (:conn store) (core/str-uuid [:foo :bar]) [(.toByteArray test-meta) (.toByteArray test-data)])
+  (get-both (:conn store) (core/str-uuid [:foo :bar]))
+  (def foo (qa/execute (:session (:conn store))
                        (qh/select :foo)))
 
   (get-data (:conn store) "06cee0d3-72f7-5bfd-ae3b-af7c2f0fef8f")
